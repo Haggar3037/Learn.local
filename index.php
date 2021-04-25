de0bc98e11b1a67a11dc1ef8e10cab3345a18af7
Haggar3037 <haggar3037@gmail.com>
2021-04-25 22:56:33 +0700

    Обучение середина первого урока

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	Гачи рулит 
-	<?
-		$a = 2;
-		$b = 3;
+<?php
 
-		echo $a + $b;
-	?>
-</body>
-</html>
+$my_variable = 100; // целое число - integer - int
+$your_variable = 500.101; //дробь  - float 
+$c = "Hawdy hlo"; // строка - string - str
+$x = $arrayName = array (1,'Jonny',3,4,5,6,r=>array(петуч,гачи)); //массив - array
+$t = true; //Булево значение - boolean, bol
+$f = false; //Булево значение
+	echo $my_variable; 
+	echo '<hr>';
+	echo $your_variable;
+	echo '<br/>';
+	echo $x[0];
+	echo '<br/>';
+	echo $arrayName[r][0];
